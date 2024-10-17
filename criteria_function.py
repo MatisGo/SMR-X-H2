0dbd1d12da84da2460e74528a61b77eb604e1c12
Matis <you@example.com>
2024-10-17 10:35:57 +0300

    Added criteria grade for elec and smr

--- criteria_function.py
+++ criteria_function.py
@@ -19,17 +19,24 @@ def criteria_ranking_function(excel_data_srm,excel_data_electrolysis,combination
                     for elec_index, elec_data in excel_data_electrolysis.items(): # This loop is too look for the name of the SMR Technology(Coupling) matching it in the SMR Data
                         callable_elec= elec_data.get('Technology')
                         if electrolysis_tech == callable_elec:
-                            
+                            #Here we have the correct values to start the Calculation of the Grade
+                            #First SMR Values
+                            smr_grade=(float(smr_data.get('Capex'))*float(criteria_weighting.get('Low Capex')))+(float(smr_data.get('Safety'))*float(criteria_weighting.get('Safety')))+(float(smr_data.get('Rentability'))*float(criteria_weighting.get('Rentability')))+(float(smr_data.get('Opex'))*float(criteria_weighting.get('Low Opex')))+(float(smr_data.get('Ecological impact'))*float(criteria_weighting.get('Ecological Impact')))+(float(smr_data.get('Startup time'))*float(criteria_weighting.get('Fast startup time')))+(float(smr_data.get('Scalability'))*float(criteria_weighting.get('Scalability')))+(float(smr_data.get('Availability (h/year)'))*float(criteria_weighting.get('High Availability (h/year)')))+(float(smr_data.get('Plant Area/Footprint'))*float(criteria_weighting.get('Plant Area/Footprint')))+(float(smr_data.get('Technology readiness'))*float(criteria_weighting.get('Technology readiness')))+(float(smr_data.get('Connection flexibility'))*float(criteria_weighting.get('Connection flexibility')))+(float(smr_data.get('Geopolitical barriers'))*float(criteria_weighting.get('Geopolitical barriers')))+(float(smr_data.get('Economic lifetime'))*float(criteria_weighting.get('Economic lifetime')))+(float(smr_data.get('Production efficiency'))*float(criteria_weighting.get('Production efficiency')))+(float(smr_data.get('Waste and decomissioning'))*float(criteria_weighting.get('Waste and decomissioning')))
+                            elec_grade=(float(elec_data.get('Capex'))*float(criteria_weighting.get('Low Capex')))+(float(elec_data.get('Safety'))*float(criteria_weighting.get('Safety')))+(float(elec_data.get('Rentability'))*float(criteria_weighting.get('Rentability')))+(float(elec_data.get('Opex'))*float(criteria_weighting.get('Low Opex')))+(float(elec_data.get('Ecological impact'))*float(criteria_weighting.get('Ecological Impact')))+(float(elec_data.get('Startup time'))*float(criteria_weighting.get('Fast startup time')))+(float(elec_data.get('Scalability'))*float(criteria_weighting.get('Scalability')))+(float(elec_data.get('Availability (h/year)'))*float(criteria_weighting.get('High Availability (h/year)')))+(float(elec_data.get('Plant Area/Footprint'))*float(criteria_weighting.get('Plant Area/Footprint')))+(float(elec_data.get('Technology readiness'))*float(criteria_weighting.get('Technology readiness')))+(float(elec_data.get('Connection flexibility'))*float(criteria_weighting.get('Connection flexibility')))+(float(elec_data.get('Geopolitical barriers'))*float(criteria_weighting.get('Geopolitical barriers')))+(float(elec_data.get('Economic lifetime'))*float(criteria_weighting.get('Economic lifetime')))+(float(elec_data.get('Production efficiency'))*float(criteria_weighting.get('Production efficiency')))+(float(elec_data.get('Waste and decomissioning'))*float(criteria_weighting.get('Waste and decomissioning')))
+                            # A wheighting is needed
+                            final_grade= smr_grade + elec_grade #UPDATE THIS 
+                            grade_info ={
+                                'Grade': final_grade
+                            }
+                            combination.update(grade_info)
+            
+                            print('Grade for',callable_smr)
+                            print(smr_grade)
+                            print(combination)
 
-                            print('ici')
-                            print(callable_smr,callable_elec)
-
-
-
-
-
-                            
-        return 
+     
+        return combinations
 
     except Exception as e:
         print(f"Error during criteria ranking: {e}")
+
